2370dda0dc41e616ab3095f1b12bb2bfcac7d19e
Yousef <gheibiyousef2000@gmail.com>
2021-12-29 22:56:34 +0330

    change the cart layout

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -4,6 +4,7 @@ import {ProductListModel} from "../../models/productlist.model";
 import {ProductModel} from "../../models/product.model";
 import {CartServiceService} from "../../services/cart-service.service";
 import {share, shareReplay} from "rxjs";
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -19,7 +20,7 @@ export class CartComponent implements OnInit {
   show: boolean = false;
   totalPrice: number = 0;
 
-  constructor(private cartService: CartServiceService,
+  constructor(public router : Router,private cartService: CartServiceService,
               public dialog: MatDialog) {
     this.show = this.cartService.show;
   }
@@ -40,11 +41,6 @@ export class CartComponent implements OnInit {
     this.cartService.removeCartItem(item);
   }
 
-  emptyCart() {
-    this.cartService.removeAllCart();
-    this.show = false;
-  }
-
   addMore(product: ProductModel) {
     this.cartService.addMore(product);
   }
@@ -61,4 +57,17 @@ export class CartComponent implements OnInit {
       }
     }
   }
+
+  addShopping(){
+    this.router.navigate(['/products']);
+  }
+
+  exportFactor(){
+    this.cartService.productList
+      .subscribe(res => {
+        this.selectedRow = res;
+        console.log(this.selectedRow);
+          })
+
+        }
 }
