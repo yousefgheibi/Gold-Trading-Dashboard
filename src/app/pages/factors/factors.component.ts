405f492f5aa48e64640b0374b86e03201108100a
Yousef <gheibiyousef2000@gmail.com>
2022-01-03 11:16:39 +0330

    convert type of exporting excel for solve bug (form table to json)

--- src/app/pages/factors/factors.component.ts
+++ src/app/pages/factors/factors.component.ts
@@ -79,11 +79,12 @@ export class FactorsComponent implements OnInit {
   }
 
   exportExcel():void{
-    let element = document.getElementById('excel-table');
-    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    const ws: XLSX.WorkSheet =XLSX.utils.json_to_sheet(this.invoiceList);
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
     XLSX.writeFile(wb, this.fileName +'.xlsx');
+   
+    
 
   }
 
