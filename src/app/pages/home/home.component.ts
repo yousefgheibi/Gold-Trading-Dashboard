1d2d81aea9a59729d68cd12ceb7d67f9458a3906
Yousef <gheibiyousef2000@gmail.com>
2022-01-03 21:07:44 +0330

    finish home-page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InvioceModel } from 'src/app/models/invoice.model';
+import { ProductModel } from 'src/app/models/product.model';
+import { InvoiceApiService } from 'src/app/services/invoice-api.service';
+import { ProductApiService } from 'src/app/services/product-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  productData: ProductModel[] = [];
+  factorData: InvioceModel[] = [];
+  constructor(private _productApi: ProductApiService, private _invoiceApi : InvoiceApiService) { }
 
   ngOnInit(): void {
+    this.getProduct();
+    this.getFacors();
   }
 
+
+  getProduct() {
+    this._productApi.getProduct().subscribe(
+      res => {
+        this.productData = res;
+      })
+  }
+
+  getFacors() {
+    this._invoiceApi.getInvoices().subscribe(
+      res => {
+        this.factorData = res;
+      }
+    )
+  }
 }
